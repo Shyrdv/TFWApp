9f01459081b3a2a14667406c66ce8bc7cdb58d05
Daviid Valentino Bernth <davi9400@edu.eal.dk>
2020-03-04 13:17:15 +0100

    Added NavigationDrawer Button

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.garden.navigationdrawer import NavigationDrawer as ND
 
 
@@ -14,7 +18,7 @@ class LoginPage(Screen):
         user = self.ids.login
         pwd = self.ids.passw
         info = self.ids.info
-
+        i = 0
         username = user.text
         password = pwd.text
 
@@ -24,14 +28,16 @@ class LoginPage(Screen):
             if username == 'admin' and password == 'admin':
                 info.text = ''
                 self.manager.current = "user"
+
             else:
                 info.text = '[color=#FF0000]Invalid Username and/or Password[/color]'
 
 
+
+
 class UserPage(Screen):
     pass
-  #  def __init__(self, **kwargs):
-        #self.add_widget(NavDemoWindow)
+
 
 class ScreenManagement(ScreenManager):
     pass
