1a5d78fa785561a029ba409292cfcf62364507c3
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2020-03-13 14:38:37 +0100

    TestTilAPK

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.garden.navigationdrawer import NavigationDrawer as ND
+from kivy.garden.navigationdrawer import NavigationDrawer
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.metrics import dp, sp
 
 
-class NavDemoWindow(ND):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class NavDemoWindow(NavigationDrawer):
+    pass
 
 
 class LoginPage(Screen):
     def verify_credentials(self):
+
         user = self.ids.login
         pwd = self.ids.passw
         info = self.ids.info
         r_user = self.ids.rem_user
         r_pass = self.ids.rem_pass
 
-        i = 0
         username = user.text
         password = pwd.text
 
@@ -42,8 +44,6 @@ class LoginPage(Screen):
                 info.text = '[color=#FF0000]Invalid Username and/or Password[/color]'
 
 
-
-
 class UserPage(Screen):
     pass
 
@@ -61,4 +61,6 @@ class LoginApp(App):
 
 
 if __name__ == '__main__':
+    Config.set('graphics', 'resizable', 0)
+    Window.size = (500, 1000)
     LoginApp().run()
