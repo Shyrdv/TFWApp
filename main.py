03cd02ffe1e37672ec080c0c2ab00508760e198e
Daviid Valentino Bernth <davi9400@edu.eal.dk>
2020-03-19 13:42:58 +0100

    Fixed Admin rights in database

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ db = mysql.connector.connect(
 )
 
 mycursor = db.cursor()
-
+#mycursor.execute("DROP TABLE Users")
 #mycursor.execute("CREATE TABLE Users (username VARCHAR(20) NOT NULL, password VARCHAR(100) NOT NULL, admin_rights boolean NOT NULL, userID int PRIMARY KEY AUTO_INCREMENT)")
 
 hash_object2 = hashlib.sha256('van'.encode('utf-8'))
 hashedpw2 = hash_object2.hexdigest()
 
-#mycursor.execute("INSERT INTO Users (username, password, admin_rights) VALUES (%s,%s,%s)", ("van", hashedpw2, True))
-#db.commit()
+mycursor.execute("INSERT INTO Users (username, password, admin_rights) VALUES (%s,%s,%s)", ("van", hashedpw2, False))
+db.commit()
 
 
 class NavDemoWindow(NavigationDrawer):
