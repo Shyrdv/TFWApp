59540e36a5966ebae07cae1e044f6c424cdc8596
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2020-03-26 10:51:03 +0100

    med knapper, men grimt.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.garden.navigationdrawer import NavigationDrawer
 from kivy.config import Config
 from kivy.core.window import Window
 from kivy.metrics import dp, sp
